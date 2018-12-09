532f1a1f7e7592e38111a9af43c6379637d48b9b
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2018-12-08 22:28:05 -0800

    feat: add session player validation to session creation

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,4 +3,5 @@ class Session < ApplicationRecord
   validates_associated :game
 
   has_many :session_players
+  validates_presence_of :session_players
 end
--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -4,6 +4,8 @@ class SessionTest < ActiveSupport::TestCase
   test "should succeed when session is valid" do
     game = games(:one)
     session = Session.new(game: game)
+    player = session_players(:one)
+    session.session_players << player
 
     assert session.valid?
   end
@@ -13,5 +15,13 @@ class SessionTest < ActiveSupport::TestCase
 
     refute session.valid?
     assert_not_nil session.errors[:game]
+  end
+
+  test "should fail when missing at least one player" do
+    session = Session.new()
+    session.session_players = Array.new
+  
+    refute session.valid?
+    assert_not_empty session.errors[:session_players]
   end
 end
