6bbf7eff765e8ed368db47c4a0d83da5972e00e6
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2019-03-25 21:35:34 -0700

    feat: scaffold new user session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :players
   resources :games
   resources :sessions
+  resources :user_sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
