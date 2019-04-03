bfd5d874c2fdf201f1e4624120bb74aff08d6eea
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2019-04-02 23:03:57 -0700

    feat: verify ties in game player stats calculation

--- test/models/concerns/games/player_stats_test.rb
+++ test/models/concerns/games/player_stats_test.rb
@@ -11,14 +11,38 @@ class PlayerStatsTest < ActiveSupport::TestCase
 
       assert_includes [winning_player], game.best_player.player
     end
+    
+    test "should return a player as best player when tied for best" do
+      game = create_game
+      winning_player1 = create_player
+      winning_player2 = create_player
+
+      create_session(game, winning_players: [winning_player1])
+      create_session(game, winning_players: [winning_player2])
+
+      assert_includes [winning_player1, winning_player2], game.best_player.player
+    end
 
     test "should return a player as worst player" do
       game = create_game
       losing_player = create_player
       winning_player = create_player
 
-      create_session(game, winning_players: [winning_player], losing_players: [losing_player])
+      create_session(game, winning_players: [winning_player],
+                     losing_players: [losing_player])
 
       assert_includes [losing_player], game.worst_player.player
+    end
+
+    test "should return a player as worst player when tied for best" do
+      game = create_game
+      winning_player = create_player
+      losing_player1 = create_player
+      losing_player2 = create_player
+
+      create_session(game, winning_players: [winning_player], 
+                    losing_players: [losing_player1, losing_player2])
+
+      assert_includes [losing_player1, losing_player2], game.worst_player.player
     end
 end
