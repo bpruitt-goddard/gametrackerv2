084b2a5efb983433e2ca67d73732c91df5067f74
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2019-05-01 22:51:31 -0700

    feat: add player worst game edge case tests

--- test/models/concerns/players/game_stats_test.rb
+++ test/models/concerns/players/game_stats_test.rb
@@ -72,4 +72,28 @@ class GameStatsTest < ActiveSupport::TestCase
         assert_equal game, worst_game.game
         assert_equal 0.25, worst_game.win_percent
     end
+
+    test "should choose an arbitrary game when tied for worst game" do
+        player = create_player
+        game_one = create_game
+        game_two = create_game ('other')
+        create_session(game_one, losing_players: [player])
+        create_session(game_two, losing_players: [player])
+
+        worst_game = player.worst_game
+
+        assert_includes [game_one, game_two], worst_game.game
+        assert_equal 0, worst_game.win_percent
+    end
+
+    test "should choose worst placing in game when playing as multiple players for worst game" do
+        player = create_player
+        game = create_game
+        create_session(game, winning_players: [player], losing_players: [player])
+
+        worst_game = player.worst_game
+
+        assert_equal game, worst_game.game
+        assert_equal 0, worst_game.win_percent
+    end
 end
