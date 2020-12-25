f3c0c8033c3dee8eee60f6b337d4f2301d16019d
Brian Pruitt-Goddard <bpruitt-goddard@github>
2020-12-24 21:27:58 -0800

    feat: Return search stats
    when values supplied for search parameters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,17 @@
 class SearchController < ApplicationController
+  def search
     @games = Game.all
     @players = Player.all
+    @params = search_params
+    
+    if @params[:player_id].present? && @params[:game_id].present? 
+      @search_stats = Search.new(search_params).search_stats
+    end
   end
 
+  private
+    def search_params
+      params
+        .permit(:player_id, :game_id)
+    end
 end
