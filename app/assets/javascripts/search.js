a6d1fa1b1587cf85697131553f71c7cb25053bc8
Brian Pruitt-Goddard <bpruitt-goddard@github>
2020-12-24 21:28:43 -0800

    Show search stats on page
    when searching

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,15 @@
 $(document).on('turbolinks:load', function() {
+    // Set initial dropdown values on load from a search
+    var initialPlayerId = $('#player_id').val();
+    if (initialPlayerId !== '') {
+        setDisplayPlayerNameFromId(initialPlayerId);
+    }
+    
+    var initialGameId = $('#game_id').val();
+    if (initialGameId !== '') {
+        setDisplayGameNameFromId(initialGameId);
+    }
+    
     $("#selectedGame").on('input', function () {
         setHiddenGameIdValue(this.value);
     });
@@ -26,6 +37,28 @@ $(document).on('turbolinks:load', function() {
         if (selectedPlayer.length > 0) {
             var playerId = $(selectedPlayer[0]).data('value');
             $('#player_id').val(playerId);    
+        }
+    }
+
+    function setDisplayPlayerNameFromId(playerId) {
+        var selectedPlayer = $('#players').find('option').filter(function(){
+            return this.getAttribute('data-value') == playerId;
+        });
+
+        if (selectedPlayer.length > 0) {
+            var playerName = selectedPlayer[0].value;
+            $('#selectedPlayer').val(playerName);
+        }
+    }
+
+    function setDisplayGameNameFromId(gameId) {
+        var selectedGame = $('#games').find('option').filter(function(){
+            return this.getAttribute('data-value') == gameId;
+        });
+
+        if (selectedGame.length > 0) {
+            var gameName = selectedGame[0].value;
+            $('#selectedGame').val(gameName);
         }
     }
 });
