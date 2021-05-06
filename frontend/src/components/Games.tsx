315338d18cb08b0d9522926d4ff8fff521fb46a3
Brian Pruitt-Goddard <bpruitt-goddard@github>
2021-05-05 18:20:44 -0700

    feat: Add games page and game links
    Add table of mock games and allow them to be navigated to for details

--- frontend/src/components/Games.tsx
+++ frontend/src/components/Games.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import { Table } from 'reactstrap';
+import Game from './Game';
 
-function Game() {
+function Games() {
     return (
         <div>
             <h2>Game</h2>
             Game page!
+            <Switch>
+                <Route path="/games/:id" component={Game} />
+                <Route render={() => GetGames()} />
+            </Switch>
         </div>
     );
 }
 
-export default Game;
+function GetGames() {
+    return (
+        <div>
+            <Table>
+                <thead>
+                    <th>Name</th>
+                    <th>Bgg Id</th>
+                    <th>Game Type</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    {
+                        [1,2,3,4,5].map(n => {
+                            return (
+                                <tr key={n}>
+                                    <th>
+                                        <Link to={`games/${n}`}>Game {n}</Link>
+                                    </th>
+                                    <th>{n}</th>
+                                    <th>Competitive</th>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default Games;
