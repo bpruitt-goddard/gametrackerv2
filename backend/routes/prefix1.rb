cc9e3af516cd8f4abbbf11a144e811d8c19ba91d
Brian Pruitt-Goddard <bpruitt-goddard@github>
2021-06-05 15:05:18 -0700

    fix: replace existing roda app with simplified version
    Start from smaller boilerplate for simplicity

--- backend/routes/prefix1.rb
+++ backend/routes/prefix1.rb
@@ -1,5 +1,8 @@
-class App
+class GameTracker
   hash_routes.on 'prefix1' do |r|
-    set_view_subdir 'prefix1'
+    # set_view_subdir 'prefix1'
+    r.get "world" do
+      "hello world!"
+    end
   end
 end
