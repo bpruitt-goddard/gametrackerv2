791d2f1543bd4572b0dd6facbf67d753401ed9db
Brian Pruitt-Goddard <bpruitt-goddard@github>
2021-06-08 21:13:49 -0700

    refactor: create game list from a paramater
    instead of hard coding. Will then be moved to API call

--- frontend/src/components/Games.tsx
+++ frontend/src/components/Games.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import { Table } from 'reactstrap';
 import Game from './Game';
 
+interface game {
+    id: number;
+    name: string;
+    bgg_id: number;
+    game_type: string;
+}
+
 function Games() {
+
+    const [list, setList] = useState<game[]>([]);
+
     return (
         <div>
             <h2>Game</h2>
@@ -13,36 +24,40 @@ function Games() {
             </Switch>
         </div>
     );
+
+    function GetGames() {
+        return (
+            <div>
+                <Table>
+                    <thead>
+                        <th>Name</th>
+                        <th>Bgg Id</th>
+                        <th>Game Type</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        {
+                            list.map(g => {
+                                return (
+                                    <tr key={g.id}>
+                                        <th>
+                                            <Link to={`games/${g}`}>{g.name}</Link>
+                                        </th>
+                                        <th>{g.id}</th>
+                                        <th>{g.game_type}</th>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
 }
 
-function GetGames() {
-    return (
-        <div>
-            <Table>
-                <thead>
-                    <th>Name</th>
-                    <th>Bgg Id</th>
-                    <th>Game Type</th>
-                    <th></th>
-                </thead>
-                <tbody>
-                    {
-                        [1,2,3,4,5].map(n => {
-                            return (
-                                <tr key={n}>
-                                    <th>
-                                        <Link to={`games/${n}`}>Game {n}</Link>
-                                    </th>
-                                    <th>{n}</th>
-                                    <th>Competitive</th>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
-            </Table>
-        </div>
-    )
-}
+// function Games() {
+// }
+
 
 export default Games;
