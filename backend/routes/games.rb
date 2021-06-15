8f220afae11ac13c40dc81225e3a204ab9f20900
Brian Pruitt-Goddard <bpruitt-goddard@github>
2021-06-14 17:04:43 -0700

    feat: Add api for retrieving game by id

--- backend/routes/games.rb
+++ backend/routes/games.rb
@@ -1,5 +1,15 @@
 class App
   hash_branch('games') do |r|
+    r.is Integer do |id|
+      @game = Game[id]
+
+      r.get do
+        @game.to_hash
+      end
+
+      # TODO: add delete here
+    end
+
     r.get do
       Game.all
     end
