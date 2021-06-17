c8674f9b342bc01a225080f2434a31ecf751433b
Brian Pruitt-Goddard <bpruitt-goddard@github>
2021-06-16 18:54:47 -0700

    feat: Retrieve existing game for add edit page
    convert page to a form to start building out form

--- frontend/src/components/game/AddEdit.tsx
+++ frontend/src/components/game/AddEdit.tsx
@@ -1,17 +1,47 @@
-import { FC, useState } from 'react';
-import { RouteComponentProps } from "react-router-dom";
+import { FC, useEffect, useState } from 'react';
+import { Link, RouteComponentProps } from "react-router-dom";
+import { getGame } from '../../services/games';
 import { IGame } from '../../services/type';
 import { IGameProps } from './IGameProps';
 
 const AddEdit : FC<RouteComponentProps<IGameProps>> = props => {
 	const { id } = props.match.params;
+	const isAddMode = !id;
 	const [game, setGame] = useState<IGame>({name: 'Gloom', id: 4, bgg_id: 123, game_type: 'competitive'});
 
+	useEffect(() => {
+		if (isAddMode)
+			return;
+
+		getGame<IGame>(+id)
+          .then(game => {
+            setGame(game);
+        })
+	}, [])
+
+
+	function onSubmit() {
+	}
+
 	return (
-	    <div>
-		    Editing game {id}<br />
-		    Game Name: {game.name}
-	    </div>
+		<form onSubmit={onSubmit}>
+			<h1>{isAddMode ? "New Game" : game.name}</h1>
+			<div className="form-group">
+				<button
+					type="submit"
+					className="btn btn-primary">
+					Save
+				</button>
+				<Link
+					to={isAddMode ? '.' : '..'}
+					className="btn btn-link"
+					>
+					Cancel
+				</Link>
+
+			</div>
+
+		</form>
 	);
 }
 
