4616e2e274c8c30f6b63153630c54e5735541db8
Brian Pruitt-Goddard <bpruitt-goddard@github>
2021-06-16 17:29:45 -0700

    feat: Add post/create support to games api

--- backend/routes/games.rb
+++ backend/routes/games.rb
@@ -13,5 +13,25 @@ class App
     r.get do
       Game.all
     end
+
+    r.post do
+      @game = Game.new(game_params(r))
+
+      if @game.valid? && @game.save
+        @game.to_hash
+      else
+        @game.errors
+      end
+    end
   end
+
+  private
+    def game_params(r)
+      params = JSON.parse(r.body.read)
+      {
+        name: params['name'],
+        bgg_id: params['bgg_id'],
+        game_type: params['game_type']
+      }
+    end
 end
